9b931d517316549b743531d994f91ce524a16d4d
Geldion <zugadum@mail.ru>
2024-08-11 14:50:28 +0300

    Update code. Add decorator skipIf for working with unittests skip method

--- test_runner.py
+++ test_runner.py
@@ -3,18 +3,23 @@ import unittest
 
 
 class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_walk(self):
         test_runner = runner.Runner('Jack')
         for _ in range(10):
             test_runner.walk()
         unittest.TestCase.assertEqual(self, test_runner.distance, 50)
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_run(self):
         test_runner = runner.Runner('Fred')
         for _ in range(10):
             test_runner.run()
         unittest.TestCase.assertEqual(self, test_runner.distance, 100)
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_challenge(self):
         test_runner_1 = runner.Runner('Harold')
         test_runner_2 = runner.Runner('Henry')
